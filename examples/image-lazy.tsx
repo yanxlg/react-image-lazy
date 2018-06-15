ae79d8f4a405fbae88394f9f21a635935cc59191
yanxianliang <798410225@qq.com>
2018-06-15 22:17:33 +0800

    add exdample

---
+++ examples/image-lazy.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+class ImageLazyloadDemo extends React.Component{
+    render(){
+        return <div>1</div>
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <ImageLazyloadDemo />
+    </div>,
+    document.getElementById('__react-content'),
+);
+
+export default ImageLazyloadDemo;
